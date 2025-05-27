cb3ea86ba784e9bf587dfae77faac4a7c94471af
Maria Martinas <105001698+mariamartinas26@users.noreply.github.com>
2025-05-27 15:46:06 +0300

    initial

---
+++ workoutTracker/src/test/java/com/marecca/workoutTracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marecca.workoutTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
