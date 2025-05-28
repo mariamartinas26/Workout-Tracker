b91411835c7cacee03dcb8fb1bd0b3d62a09bca9
Maria Martinas <105001698+mariamartinas26@users.noreply.github.com>
2025-05-29 01:52:36 +0300

    solved update profile

--- workoutTracker/src/main/java/com/marecca/workoutTracker/WorkoutTrackerApplication.java
+++ workoutTracker/src/main/java/com/marecca/workoutTracker/WorkoutTrackerApplication.java
@@ -2,12 +2,11 @@ package com.marecca.workoutTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class WorkoutTrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutTrackerApplication.class, args);
 	}
-
 }
--- workoutTracker/src/main/java/com/marecca/workoutTracker/config/SecurityConfig.java
+++ workoutTracker/src/main/java/com/marecca/workoutTracker/config/SecurityConfig.java
@@ -18,12 +18,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
+
         return http.build();
     }
 }
